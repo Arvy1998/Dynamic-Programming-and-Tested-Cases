36b556d2da98a08c982197d9ee6ccdb2a9d237b7
Arvy1998 <35611633+Arvy1998@users.noreply.github.com>
2018-05-09 00:28:21 +0300

    Create Fibonacci Matrix Exponentation.cpp

---
+++ Fibonacci Matrix Exponentation.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <map>
+
+using namespace std;
+
+map <unsigned long long, unsigned  long long> Fib;
+
+unsigned long long fibonaci(unsigned long long n) {
+	if (Fib.count(n)) {
+		return Fib[n];
+	}
+	unsigned long long k = n / 2;
+	if (n % 2 == 0) {
+		return Fib[n] = (fibonaci(k) * fibonaci(k) + fibonaci(k - 1) * fibonaci(k - 1)) % 1000000007;
+	}
+	else {
+		return Fib[n] = (fibonaci(k) * fibonaci(k + 1) + fibonaci(k - 1) * fibonaci(k)) % 1000000007;
+	}
+}
+
+int main() {
+	unsigned long long n;
+	cin >> n;
+	Fib[0] = 1;
+	Fib[1] = 1;
+	if (n == 0) {
+		cout << "0";
+	}
+	else {
+		cout << fibonaci(n - 1);
+	}
+}
